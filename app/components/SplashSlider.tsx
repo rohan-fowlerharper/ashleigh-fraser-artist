a5d9e019690812cb58fc80bd0c767642db9742d0
rohan-fowlerharper <rohan.fowlerharper@gmail.com>
2022-05-10 23:48:07 +1200

    fix(css): width on mobile

--- app/components/SplashSlider.tsx
+++ app/components/SplashSlider.tsx
@@ -19,7 +19,7 @@ SwiperInterface.use([Pagination, A11y, Autoplay]);
 export default function SplashSlider() {
   return (
     <div className="mx-auto flex max-w-4xl flex-col md:max-h-full md:flex-row">
-      <div className="flex aspect-[3/4] h-auto flex-col  items-center justify-center bg-zinc-50 p-8 text-center xs:mx-auto xs:w-[384px] sm:mx-auto sm:w-[384px] md:w-1/2">
+      <div className="flex aspect-[3/4] h-auto w-full flex-col items-center justify-center bg-zinc-50 p-8 text-center xs:mx-auto xs:w-[384px] sm:mx-auto sm:w-[384px] md:w-1/2">
         <span className="h-full text-[7.5vw] font-medium text-zinc-600 xs:text-3xl sm:text-[2rem] md:leading-[2.75rem] lg:text-[2.4rem]">
           My hand drawn, bespoke commission process starts with your own photo.
           I transform the photo into an outline, then gradually build up detail
@@ -27,7 +27,7 @@ export default function SplashSlider() {
           treasure for a lifetime.
         </span>
       </div>
-      <div className="xs:mx-auto xs:w-[384px] sm:mx-auto sm:w-[384px] md:w-1/2">
+      <div className="w-full xs:mx-auto xs:w-[384px] sm:mx-auto sm:w-[384px] md:w-1/2">
         <Swiper {...swiperParams}>
           <SwiperSlide>
             <div className="aspect-[3/4] h-auto w-full bg-slate-500"></div>
