e5788dc8a84bc75736dee5f880dffee2b07abeb0
rohan-fowlerharper <rohan.fowlerharper@gmail.com>
2022-05-13 10:44:56 +1200

    experiment

--- server.js
+++ server.js
@@ -1,4 +1,5 @@
 import { createRequestHandler } from "@remix-run/vercel";
 import * as build from "@remix-run/dev/server-build";
 
+console.log("server.js");
 export default createRequestHandler({ build, mode: process.env.NODE_ENV });
