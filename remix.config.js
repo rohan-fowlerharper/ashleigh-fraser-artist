f29fa6a1e11cde8ab5f02caba444feb4d4a51dcf
rohan-fowlerharper <rohan.fowlerharper@gmail.com>
2022-05-13 09:50:50 +1200

    configure for node 18

--- remix.config.js
+++ remix.config.js
@@ -1,9 +1,11 @@
 /**
- * @type {import('@remix-run/dev/config').AppConfig}
+ * @type {import('@remix-run/dev').AppConfig}
  */
 module.exports = {
+  serverBuildTarget: "vercel",
   appDirectory: "app",
   assetsBuildDirectory: "public/build",
+  server: process.env.NODE_ENV === "development" ? undefined : "./api/index.js",
   publicPath: "/build/",
   serverBuildDirectory: "api/_build",
   ignoredRouteFiles: [".*"],
