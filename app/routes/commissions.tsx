03d21c3ee02eb4d26a88997026c92cd420b01a24
rohan-fowlerharper <rohan.fowlerharper@gmail.com>
2022-06-01 19:56:17 +1200

    fix(): add wip message

--- app/routes/commissions.tsx
+++ app/routes/commissions.tsx
@@ -9,11 +9,34 @@ export const meta: MetaFunction = () => {
 
 export default function CommissionsRoute() {
   return (
-    <SectionLayout id="commissions">
-      <SectionHeading
-        title="Commisssions"
-        subtitle="This is the commissions section"
-      />
-    </SectionLayout>
+    <>
+      <SectionLayout id="commissions">
+        <SectionHeading
+          title="Commisssions"
+          subtitle="More information about commissions are coming soon!"
+        />
+      </SectionLayout>
+      <div className="z-0 my-auto mb-6 flex h-32 justify-center bg-slate-100 text-center">
+        <h1 className="max-w-2xl self-center text-sm font-medium tracking-widest text-zinc-700 sm:text-base">
+          Thanks for visiting my website! It is currently under construction,
+          and this section isn't quite done. Email me at{" "}
+          <a
+            className="hover:underline"
+            href="mailto:ashleighfraser.artist@gmail.com"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            ashleighfraser.artist@gmail.com
+          </a>{" "}
+          and check out my Instagram at{" "}
+          <a
+            className="hover:underline"
+            href="https://www.instagram.com/ashleighfraser_art/"
+          >
+            @ashleighfraser_art
+          </a>{" "}
+        </h1>
+      </div>
+    </>
   );
 }
